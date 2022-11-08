793d782d4dee56a4e01b4ba24358bf4b7935e14c
santosh90360 <santosh3033@gmail.com>
2022-11-08 23:49:06 +0530

    Add project files.

---
+++ AdminModule/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdminModule.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
