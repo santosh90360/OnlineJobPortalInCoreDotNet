eb6e872b835a4e106e18066e4163f079b85e0ba8
Santosh Singh <santosh3033@gmail.com>
2022-11-10 17:09:59 +0530

    Added ApplicationDbContext

---
+++ JobSeeker/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using JobSeeker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobSeeker.DbContexts
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Registration> Registrations { get; set; }
+    }
+}
--- JobSeeker/Program.cs
+++ JobSeeker/Program.cs
@@ -1,9 +1,19 @@
+using JobSeeker.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// add
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+        ));
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
