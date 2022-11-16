7cc6dfaca11e09b7fef20ea624bc3cec202bd859
Santosh Singh <santosh3033@gmail.com>
2022-11-16 22:22:17 +0530

    Admin Profile image changes

--- JobSeeker/Controllers/AdminUserController.cs
+++ JobSeeker/Controllers/AdminUserController.cs
@@ -9,6 +9,8 @@ using JobSeeker.DbContexts;
 using JobSeeker.Models;
 using JobSeeker.Repository.IJobSeekerRepositories;
 using JobSeeker.Repository.JobRepository;
+using JobSeeker.Models.Dto;
+using NuGet.Protocol.Core.Types;
 
 namespace JobSeeker.Controllers
 {
@@ -16,10 +18,12 @@ namespace JobSeeker.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private IJobRepository _jobRepository;
-        public AdminUserController(ILogger<HomeController> logger, IJobRepository jobRepository)
+        private IJobSeekerRepository _repository;
+        public AdminUserController(ILogger<HomeController> logger, IJobRepository jobRepository, IJobSeekerRepository repository)
         {
             _logger = logger;
             _jobRepository = jobRepository;
+            _repository = repository;
         }
         // GET: AdminUser
         public async Task<IActionResult> Index()
@@ -154,13 +158,120 @@ namespace JobSeeker.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        public async Task<IActionResult> Dashboard()
+        public IActionResult Dashboard()
+        {
+            RegistrationDto registrationDto = new RegistrationDto();
+            registrationDto.Email = this.HttpContext.Session.GetString("Email");
+            return View(registrationDto);
+        }
+
+        public async Task<IActionResult> LogOff()
+        {
+            this.HttpContext.Session.Clear();
+            return RedirectToAction("Login", "Home");
+        }
+        [HttpGet]
+        public async Task<IActionResult> ProfileImage()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> ProfileImage(IFormFile formFile)
+        {
+            try
+            {
+                if (formFile != null && formFile.Length > 0)
+                {
+                    string profileImageName = formFile.FileName;
+                    profileImageName = Path.GetFileName(profileImageName);
+                    Guid guid = Guid.NewGuid();
+                    string newfileName = guid.ToString();
+                    string fileExtention = Path.GetExtension(formFile.FileName);
+
+                    if (formFile.ContentType != "image/png" && formFile.ContentType != "image/jpeg")
+                    {
+                        ViewBag.Message = ResultStatus.InvalidFileType;
+                        return View();
+                    }
+
+                    if (formFile.Length > 1000000)
+                    {
+                        ViewBag.Message = ResultStatus.MaxExceedFileSize;
+                        return View();
+                    }
+
+                    profileImageName = newfileName + fileExtention;
+
+                    RegistrationDto registrationDto = new RegistrationDto();
+                    registrationDto.Email = this.HttpContext.Session.GetString("Email");
+                    registrationDto.ProfileImage = SD.ProfileImagePath + profileImageName;
+                    //Upload image path in registration table
+                    var result = await _repository.UpdatePhoto(registrationDto);
+                    if (result.Result == ResultStatus.Success)
+                    {
+                        // Save file in wwwroot folder
+                        string uploadpath = Path.Combine(Directory.GetCurrentDirectory(), SD.ProfileImageRootPath, profileImageName);
+                        var stream = new FileStream(uploadpath, FileMode.Create);
+                        formFile.CopyToAsync(stream);
+                        ViewBag.Message = ResultStatus.Success;
+                        var userAfterPhotoUploaded = await _repository.GetJobSeeker(registrationDto);
+                        if (userAfterPhotoUploaded != null && userAfterPhotoUploaded.Id > 0)
+                        {
+                            HttpContext.Session.SetString("ProfileImage", userAfterPhotoUploaded.ProfileImage);
+                            return View();
+                        }
+                    }
+                    else
+                    {
+                        ViewBag.Message = "Photo does not upload successfully. Try again";
+                        return View();
+                    }
+                }
+                else
+                {
+                    ViewBag.Message = ResultStatus.IsNull;
+                    return View();
+                }
+            }
+            catch
+            {
+                ViewBag.Message = "Error while uploading the profile photo.";
+                return View();
+            }
+            return View();
+        }
+        [HttpGet]
         public async Task<IActionResult> Profile()
         {
-            return View();
+
+            RegistrationDto registrationDto = new RegistrationDto();
+            registrationDto.Email = this.HttpContext.Session.GetString("Email");
+            registrationDto = await _repository.GetJobSeeker(registrationDto);
+            return View(registrationDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Profile(string email)
+        {
+            return RedirectToAction(nameof(ProfileEdit));
+        }
+        [HttpGet]
+        public async Task<IActionResult> ProfileEdit()
+        {
+
+            RegistrationDto registrationDto = new RegistrationDto();
+            registrationDto.Email = this.HttpContext.Session.GetString("Email");
+            registrationDto = await _repository.GetJobSeeker(registrationDto);
+            return View(registrationDto);
+        }
+        [HttpPost]
+        public async Task<IActionResult> ProfileEdit(RegistrationDto registrationDto)
+        {
+            registrationDto.Email = this.HttpContext.Session.GetString("Email");
+            registrationDto = await _repository.UpdateProfileUser(registrationDto);
+            ViewBag.Message = registrationDto.Result;
+            return View(registrationDto);
         }
         private async Task<bool> JobDtoExists(int id)
         {
