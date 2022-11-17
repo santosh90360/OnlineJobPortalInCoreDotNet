6322bf42edb9ac5ab39548478aaf17b5f9953d68
Santosh Singh <santosh3033@gmail.com>
2022-11-17 11:06:23 +0530

    Company Layout, Dashboard and controller added

---
+++ JobSeeker/Controllers/CompanyAccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobSeeker.Controllers
+{
+    public class CompanyAccountController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
